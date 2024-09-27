1cbe9067db92ebf7150c7a5d69655479cb36ef0c
sven <sven.j.fijan@gmail.com>
2024-09-27 08:50:51 +0200

    rebind <leader>fb to fuzzy find in current buffer

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -6,7 +6,7 @@ return {
 			local builtin = require('telescope.builtin')
 			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+			vim.keymap.set('n', '<leader>fb', builtin.current_buffer_fuzzy_find, {})
 			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 		end
 	},
