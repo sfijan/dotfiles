2bf7355fbc8cf336c3b6b69f334683ab84e52ce5
sven <sven.j.fijan@gmail.com>
2025-02-04 16:07:26 +0100

    replaced black with ruff as python formatter

--- nvim/dot-config/nvim/lua/plugins/lsp-config.lua
+++ nvim/dot-config/nvim/lua/plugins/lsp-config.lua
@@ -22,17 +22,34 @@ return {
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
-			})
-			lspconfig.basedpyright.setup({
-				capabilities = capabilities,
+            })
+            lspconfig.basedpyright.setup({
+                capabilities = capabilities,
+                settings = {
+                    basedpyright = {
+                        -- Using Ruff's import organizer
+                        disableOrganizeImports = true,
+                    },
+                    python = {
+                        analysis = {
+                            -- Ignore all files for analysis to exclusively use Ruff for linting
+                            ignore = { '*' },
+                        },
+                    },
+                },
+
 			})
             lspconfig.gopls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.ruff.setup({
                 capabilities = capabilities,
             })
 			vim.keymap.set("n", "<leader>i", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 		end,
 	},
 }
--- nvim/dot-config/nvim/lua/plugins/none-ls.lua
+++ nvim/dot-config/nvim/lua/plugins/none-ls.lua
@@ -7,10 +7,9 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
                 -- null_ls.builtins.formatting.isort,
-                null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.prettier,
+                -- null_ls.builtins.formatting.black,
+				-- null_ls.builtins.formatting.prettier,
 			},
 		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 	end,
 }
