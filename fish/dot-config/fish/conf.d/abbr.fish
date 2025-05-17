81a5427128cd49c11bc50da1977fa575ffd48611
Sven Fijan <sven.j.fijan@gmail.com>
2025-05-17 18:56:39 +0000

    added abbreviations, ignore fzf plugin files

---
+++ fish/dot-config/fish/conf.d/abbr.fish
@@ -0,0 +1,9 @@
+# git
+abbr -a -- gs 'git status'
+abbr -a -- gd 'git diff'
+abbr -a -- ga 'git add'
+abbr -a -- gc 'git commit'
+abbr -a -- gp 'git push'
+abbr -a -- gu 'git pull'
+abbr -a -- gl 'git log --oneline --color --graph'
+abbr -a -- gb 'git branch'
