cce13b72f40b2ed177040bc28e41af77c1f360a2
Sven Fijan <sven.j.fijan@gmail.com>
2025-05-18 09:33:04 +0000

    added pretty git log

--- fish/dot-config/fish/conf.d/abbr.fish
+++ fish/dot-config/fish/conf.d/abbr.fish
@@ -5,5 +5,7 @@ abbr -a -- ga 'git add'
 abbr -a -- gc 'git commit'
 abbr -a -- gp 'git push'
 abbr -a -- gu 'git pull'
-abbr -a -- gl 'git log --oneline --color --graph'
+# abbr -a -- gl 'git log --oneline --color --graph'
+abbr -a -- gl "git log --all --graph --pretty=\
+format:'%C(magenta)%h %C(white) %an  %ar%C(auto)  %D%n%S%n'"
 abbr -a -- gb 'git branch'
