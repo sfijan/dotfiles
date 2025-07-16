bd0f8543ede8281d2ff8d0a845efa7d9cdf36be2
Sven Fijan <sven.j.fijan@gmail.com>
2025-07-16 15:05:48 +0200

    added abbrs

--- fish/dot-config/fish/conf.d/abbr.fish
+++ fish/dot-config/fish/conf.d/abbr.fish
@@ -3,9 +3,15 @@ abbr -a -- gs 'git status'
 abbr -a -- gd 'git diff'
 abbr -a -- ga 'git add'
 abbr -a -- gc 'git commit'
+abbr -a -- gco 'git checkout'
 abbr -a -- gp 'git push'
 abbr -a -- gu 'git pull'
 # abbr -a -- gl 'git log --oneline --color --graph'
 abbr -a -- gl "git log --all --graph --pretty=\
 format:'%C(magenta)%h %C(white) %an  %ar%C(auto)  %D%n%S%n'"
 abbr -a -- gb 'git branch'
+abbr -a -- gf 'git fetch --all --prune'
+
+# other
+abbr -a -- alert 'hyprctl notify 5 10000 0 "fontsize:30 Task finished"'
+abbr -a -- rg 'rg -S'
